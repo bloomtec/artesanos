002a35f4d11cf1a0155a1497206839630a78104d
Bloom <bloom@mail.bloomweb.co>
2012-01-25 17:39:58 -0500

    gitignore

---
+++ app/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'bloomweb',
+		'password' => 'rr40r900343',
+		'database' => 'artesanos',
+	);
+}
