c893f498964722c06ea39de1c2bded9d8e72ec82
panda <ricardopandales@gmail.com>
2012-02-10 14:54:28 -0500

    cambios

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,13 +1,13 @@
 <?php
 class DATABASE_CONFIG {
 
-	public $default = array(
+	public  $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => 'localhost',
+		'host' => '192.168.163.16',
 		'login' => 'jndagobe',
 		'password' => '20qtxNm6E4',
 		'database' => 'jndagobe_artesanos',
 	);
-	
+
 }
