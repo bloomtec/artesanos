070a990044680f3ced4fa2bc9afe944d81421b74
Julio César Domínguez Giraldo <jucedogi@ubuntu-vm.(none)>
2012-02-14 12:48:16 -0500

    finalizando reporte por artesanos

--- app/Controller/ReportesController.php
+++ app/Controller/ReportesController.php
@@ -31,42 +31,43 @@ class ReportesController extends AppController {
 			}
 			if(!empty($this -> request -> data['Reporte']['cedula'])) {
 				$this -> loadModel('Artesano');
-				$artesanos = $this -> Artesano -> find('all', array('conditions' => array('Artesano.art_cedula LIKE' => '%' . $this -> request -> data['Reporte']['cedula'] . '%')));
+				$artesanos = $this -> Artesano -> find('list', array('conditions' => array('Artesano.art_cedula LIKE' => '%' . $this -> request -> data['Reporte']['cedula'] . '%')));
+				$this -> loadModel('Calificacion');
+				$calificaciones = $this -> Calificacion -> find('list', array('conditions' => array('Calificacion.artesano_id' => $artesanos)));
 				
-				$conditions['DatosPersonal.id'] = $ids;
+				$conditions['DatosPersonal.calificacion_id'] = $calificaciones;
 			}
-			/*
 			if(!empty($this -> request -> data['Reporte']['fecha_de_nacimiento'])) {
-				$conditions['DatosPersonal.dat_nombres LIKE'] = '%' . $this -> request -> data['Reporte']['nombres'] . '%';
+				$conditions['DatosPersonal.dat_fecha_nacimiento'] = $this -> request -> data['Reporte']['fecha_de_nacimiento'];
 			}
 			if(!empty($this -> request -> data['Reporte']['nacionalidad'])) {
-				$conditions['DatosPersonal.dat_nombres LIKE'] = '%' . $this -> request -> data['Reporte']['nombres'] . '%';
+				$conditions['DatosPersonal.dat_nacionalidad'] = $this -> request -> data['Reporte']['nacionalidad'];
 			}
 			if(!empty($this -> request -> data['Reporte']['tipo_de_sangre'])) {
-				$conditions['DatosPersonal.dat_nombres LIKE'] = '%' . $this -> request -> data['Reporte']['nombres'] . '%';
+				$conditions['DatosPersonal.dat_tipo_de_sangre'] = $this -> request -> data['Reporte']['tipo_de_sangre'];
 			}
 			if(!empty($this -> request -> data['Reporte']['sexo'])) {
-				$conditions['DatosPersonal.dat_nombres LIKE'] = '%' . $this -> request -> data['Reporte']['nombres'] . '%';
+				$conditions['DatosPersonal.dat_sexo'] = $this -> request -> data['Reporte']['sexo'];
 			}
 			if(!empty($this -> request -> data['Reporte']['estado_civil'])) {
-				$conditions['DatosPersonal.dat_nombres LIKE'] = '%' . $this -> request -> data['Reporte']['nombres'] . '%';
+				$conditions['DatosPersonal.dat_estado_civil'] = $this -> request -> data['Reporte']['estado_civil'];
 			}
 			if(!empty($this -> request -> data['Reporte']['edad'])) {
-				$conditions['DatosPersonal.dat_nombres LIKE'] = '%' . $this -> request -> data['Reporte']['nombres'] . '%';
+				//debug(date('Y-m-d', time()));
+				//$conditions['DatosPersonal.dat_fecha_nacimiento ? BETWEEN ?'] = $this -> request -> data['Reporte']['edad'];
 			}
 			if(!empty($this -> request -> data['Reporte']['grado_de_estudio'])) {
-				$conditions['DatosPersonal.dat_nombres LIKE'] = '%' . $this -> request -> data['Reporte']['nombres'] . '%';
+				$conditions['DatosPersonal.dat_grado_de_estudio'] = $this -> request -> data['Reporte']['grado_de_estudio'];
 			}
 			if(!empty($this -> request -> data['Reporte']['hijos_mayores'])) {
-				$conditions['DatosPersonal.dat_nombres LIKE'] = '%' . $this -> request -> data['Reporte']['nombres'] . '%';
+				$conditions['DatosPersonal.dat_hijos_mayores'] = $this -> request -> data['Reporte']['hijos_mayores'];
 			}
 			if(!empty($this -> request -> data['Reporte']['hijos_menores'])) {
-				$conditions['DatosPersonal.dat_nombres LIKE'] = '%' . $this -> request -> data['Reporte']['nombres'] . '%';
+				$conditions['DatosPersonal.dat_hijos_menores'] = $this -> request -> data['Reporte']['hijos_menores'];
 			}
 			if(!empty($this -> request -> data['Reporte']['tipo_de_discapacidad'])) {
-				$conditions['DatosPersonal.dat_nombres LIKE'] = '%' . $this -> request -> data['Reporte']['nombres'] . '%';
+				$conditions['DatosPersonal.dat_tipo_de_discapacidad'] = $this -> request -> data['Reporte']['tipo_de_discapacidad'];
 			}
-			 */
 			/**
 			 * ----------------------------------------------------------------------------------------------
 			 */
@@ -75,7 +76,12 @@ class ReportesController extends AppController {
 			$this -> redirect(array('controller' => 'datos_personales', 'action' => 'reporteArtesanos'));
 		}
 		$tipos_de_discapacidad = $this -> Reporte -> getValores(6);
-		$this -> set(compact('tipos_de_discapacidad'));
+		$nacionalidades = $this -> Reporte -> getValores(1);
+		$tipos_de_sangre = $this -> Reporte -> getValores(2);
+		$sexos = $this -> Reporte -> getValores(5);
+		$estados_civiles = $this -> Reporte -> getValores(3);
+		$grados_de_estudio = $this -> Reporte -> getValores(4);
+		$this -> set(compact('tipos_de_discapacidad', 'nacionalidades', 'tipos_de_sangre', 'sexos', 'estados_civiles', 'grados_de_estudio'));
 	}
 	
 	public function reporteCalificacionesOperador() {
