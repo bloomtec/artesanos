99f67abaf9912cd1dbec1d8fb694fbb96c881d39
unknown <NELSON@.(none)>
2012-03-26 17:20:36 -0500

    cambio base de datos

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,6 +1,6 @@
 <?php
 class DATABASE_CONFIG {
-/*
+
 	public  $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
@@ -10,8 +10,8 @@ class DATABASE_CONFIG {
 		'database' => 'jndagobe_artesanos',
 		'encoding' => 'utf8'
 	); 	
-	*/
 	
+	/*
 	 public  $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
@@ -23,5 +23,5 @@ class DATABASE_CONFIG {
 		'database' => 'jndagobe_artesanos',
 		'encoding' => 'utf8'
 	);
-	 
+	 */
 }
