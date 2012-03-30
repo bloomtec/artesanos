32bc1822ebf4e8e0c6debe9aa0c579ff21bec1d6
RAP <ricardopandales@bloomweb.co>
2012-03-30 11:48:46 -0500

    revs

---
+++ app/Model/Archivo.php
@@ -0,0 +1,14 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * Archivo Model
+ *
+ */
+class Archivo extends AppModel {
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'nombre';
+}
