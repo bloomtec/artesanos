bb79de6db356d63764a543237d69c37559c37277
RAP <ricardopandales@bloomweb.co>
2012-04-13 10:57:32 -0500

    revs

--- app/Model/Factura.php
+++ app/Model/Factura.php
@@ -47,7 +47,36 @@ class Factura extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+		'fac_comprobante_deposito' => array(
+			'validarComprobanteDeposito' => array(
+				'rule' => array('validarComprobanteDeposito'),
+				'message' => 'El número de comprobante debe ser único',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
 	);
+	
+	/**
+	 * Método de validación del comprobante de factura
+	 * Se valida que si hay un comprobante de deposito ingresado que este sea único.
+	 * @return : true si es un comprobante único, false en caso contrario.
+	 */
+	public function validarComprobanteDeposito() {
+		$this -> recursive = -1;
+		$factura = null;
+		$this -> data['Factura']['fac_comprobante_deposito'] = trim($this -> data['Factura']['fac_comprobante_deposito']);
+		if(!empty($this -> data['Factura']['fac_comprobante_deposito'])) {
+			$factura = $this -> findByFacComprobanteDeposito($this -> data['Factura']['fac_comprobante_deposito']);
+		}
+		if($factura) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
