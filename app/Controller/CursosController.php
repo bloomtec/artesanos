aebabcc183dd5e6ee68d51af78b3d53f9dbc8e94
Julio César Domínguez Giraldo <jucedogi@ubuntu-vm.(none)>
2012-04-26 10:55:11 -0500

    revs

--- app/Controller/CursosController.php
+++ app/Controller/CursosController.php
@@ -297,8 +297,9 @@ class CursosController extends AppController {
 			$conditions = array();
 			if ($pagina == false) {
 
-				$provincia = $this -> data["Reporte"]["provincia"];
-				$ciudad = $this -> data["Reporte"]["ciudad"];
+				$provincia = $this -> data["Reporte"]["provincia_id"];
+				$canton = $this -> data["Reporte"]["canton_id"];
+				$ciudad = $this -> data["Reporte"]["ciudad_id"];
 				$fechaCreacion = $this -> data["Reporte"]["fecha_creacion"];
 				$estado = $this -> data["Reporte"]["estado"];
 				$fecha1 = $this -> data["Reporte"]["fecha1"];
@@ -311,17 +312,17 @@ class CursosController extends AppController {
 				}
 
 				if (!empty($ciudad)) {
-					$idsCentrosArtesanales = $this -> CentroArtesanal -> find("list", array("conditions" => array("CentroArtesanal.ciudad_id" => $ciudad)));
+					$idsCentrosArtesanales = $this -> CentrosArtesanal -> find("list", array("conditions" => array("CentrosArtesanal.ciudad_id" => $ciudad)));
 					$idsSolicitudes = $this -> Curso -> Solicitud -> find("list", array("conditions" => array("Solicitud.centros_artesanal_id" => $idsCentrosArtesanales)));
 					$conditions[] = array('Curso.solicitud_id' => $idsSolicitudes);
 				}
 
 				if (!empty($fechaCreacion)) {
-					$conditions[] = array('CentrosArtesanal.created' => $fechaCreacion);
+					$conditions[] = array('Curso.created BETWEEN ? AND ?' => array($fechaCreacion . ' 00:00:00', $fechaCreacion . ' 23:59:59'));
 				}
 
 				if (!empty($estado)) {
-					$conditions[] = array('CentrosArtesanal.cur_activo' => $estado);
+					$conditions[] = array('Curso.cur_activo' => $estado);
 				}
 
 				if ($fecha1 != null && $fecha2 != null) {
@@ -345,9 +346,9 @@ class CursosController extends AppController {
 					$conditions[] = array('Curso.created between ? and ?' => array($fecha1, $fecha2));
 
 				} else if ($fecha1 != null) {
-					$conditions[] = array('Curso.created >=' => $fecha1);
+					$conditions[] = array('Curso.cur_fecha_de_inicio =' => $fecha1);
 				} else if ($fecha2 != null) {
-					$conditions[] = array('Curso.created <=' => $fecha2);
+					$conditions[] = array('Curso.cur_fecha_de_fin <=' => $fecha2);
 				}
 			}
 
@@ -370,8 +371,11 @@ class CursosController extends AppController {
 			$this -> Session -> write('numAlumnos', $numAlumnos);
 			$this -> set(compact('reporteCursos', 'reporte', 'numAlumnos'));
 		} else {
-			$idsProvincias = $this -> CentrosArtesanal -> find("list", array("fields" => array("provincia_id")));
-			$provincias = $this -> CentrosArtesanal -> Provincia -> find("list", array("conditions" => array("Provincia.id" => $idsProvincias)));
+			$provincias = array(0 => 'Seleccione...');
+			$provincias_tmp = $this -> Curso -> Provincia -> find('list');
+			foreach ($provincias_tmp as $key => $value) {
+				$provincias[$key] = $value;
+			}
 			$this -> set(compact('provincias', 'reporte'));
 		}
 
