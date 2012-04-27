69a4e647e445c3aada0a734915a4de3e08944064
Julio César Domínguez Giraldo <jucedogi@ubuntu-vm.(none)>
2012-04-27 12:16:15 -0500

    resvs

--- app/Config/database.php
+++ app/Config/database.php
@@ -1,6 +1,6 @@
 <?php
 class DATABASE_CONFIG {
-	/*	public  $default = array(
+	public  $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
@@ -10,7 +10,7 @@ class DATABASE_CONFIG {
 		'database' => 'jndagobe_artesanos',
 		'encoding' => 'utf8'
 	);
-	*/
+/*
 
   public  $default = array(
 		'datasource' => 'Database/Mysql',
@@ -23,7 +23,7 @@ class DATABASE_CONFIG {
 		'database' => 'jndagobe_artesanos',
 		'encoding' => 'utf8'
 	);
-  
+  */
 }
 
 ?>
